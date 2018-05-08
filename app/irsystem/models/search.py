7d115851eef46f52ab2b6bed4d4742c02ead8085
Sidd Srinivasan <siddhartha.srinivasan@gmail.com>
2018-05-08 01:15:45 -0400

    need for speed

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -14,9 +14,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-BASE = "/app/app/irsystem/models/"
 
-def tokenize_query(query, ds):
+########## change back for docker ###################
+BASE = "app/irsystem/models"
+
+def tokenize_query(query, ds, vocab_to_ix, words_compressed, docs_compressed, ATN_word_to_ix):
     """
     Returns a dictionary with structure {term : frequency}. Also preprocesses
     the input query string using the Sklearn TfidfVectorizer.
@@ -25,22 +27,21 @@ def tokenize_query(query, ds):
     helper = TfidfVectorizer(min_df=3, stop_words='english',  dtype=np.int16)
     tfidf_preprocessor = helper.build_preprocessor()
     tfidf_tokenizer = helper.build_tokenizer()
-    with open(os.path.join(BASE, os.path.join(ds, 'vocab_to_ix.json'))) as f:
-        vocab_to_ix= json.load(f)
-        prepro_q = tfidf_preprocessor(query)
-        q_tokens = tfidf_tokenizer(prepro_q)
-        gc.collect()
-        query_dict_ix = defaultdict(int)
-        query_dict_term = defaultdict(int)
-        for tok in q_tokens:
-            tfidf_vocab_ix = vocab_to_ix.get(tok, -1)
-            if tfidf_vocab_ix != -1:
-                query_dict_ix[vocab_to_ix[tok]] += 1
-                query_dict_term[tok] += 1
-        expanded_query_dict = expand_query(query_dict_ix, query_dict_term, vocab_to_ix)
-        f.close()
-        gc.collect()
-        return expanded_query_dict
+    prepro_q = tfidf_preprocessor(query)
+    q_tokens = tfidf_tokenizer(prepro_q)
+    gc.collect()
+    query_dict_ix = defaultdict(int)
+    query_dict_term = defaultdict(int)
+    for tok in q_tokens:
+        tfidf_vocab_ix = vocab_to_ix.get(tok, -1)
+        if tfidf_vocab_ix != -1:
+            query_dict_ix[vocab_to_ix[tok]] += 1
+            query_dict_term[tok] += 1
+    print >> sys.stderr, "lending control to expand query"
+    expanded_query_dict = expand_query(query_dict_ix, query_dict_term, vocab_to_ix, \
+     words_compressed, docs_compressed, ATN_word_to_ix)
+    gc.collect()
+    return expanded_query_dict
 
 
 def get_tfidf_of_query(query_dict, ds):
@@ -76,9 +77,9 @@ def return_relevant_red_doc_ixs(query_vec, ds, t):
         return most_rel
 
 
-def return_red_ixs(query, ds, t=5):
+def return_red_ixs(query, ds, t, red_vocab_to_ix, words_compressed, docs_compressed, ATN_word_to_ix):
     print >> sys.stderr, "return_red_ixs"
-    tokens = tokenize_query(query, ds)
+    tokens = tokenize_query(query, ds, red_vocab_to_ix, words_compressed, docs_compressed, ATN_word_to_ix)
     if tokens == {}:
         return []
     query_vec = get_tfidf_of_query(tokens, ds)
@@ -86,31 +87,25 @@ def return_red_ixs(query, ds, t=5):
     return rel_doc_ixs
 
 
-def return_reu_ids(query, ds, t=5):
+def return_reu_ids(query, ds, t, f, f2, reu_vocab_to_ix, words_compressed, docs_compressed, ATN_word_to_ix):
     print >> sys.stderr, "return_reu_ids"
-    tokens = tokenize_query(query, ds)
+    tokens = tokenize_query(query, ds, reu_vocab_to_ix, words_compressed, docs_compressed, ATN_word_to_ix)
     if tokens == {}:
         return []
     query_vec = get_tfidf_of_query(tokens, ds)
-    rel_doc_ixs = return_relevant_reu_doc_ids(query_vec, ds, t)
+    rel_doc_ixs = return_relevant_reu_doc_ids(query_vec, ds, t, f, f2)
     return rel_doc_ixs
 
+##################################################################################################
+def return_relevant_reu_doc_ids(query_vec, ds, t, f, f2):
+    print >> sys.stderr, "return_relevant_reu_doc_ids NEW"
 
-def return_relevant_reu_doc_ids(query_vec, ds, t):
-    print >> sys.stderr, "return_relevant_reu_doc_ids"
-    with open(os.path.join(BASE, os.path.join(ds, 'tfidf_mat.npz'))) as f:
-        tfidf_mat= load_npz(f)
-        cos_sim = (tfidf_mat.dot(query_vec.T)).T
-        gc.collect()
-        most_rel = zip(cos_sim.data, cos_sim.nonzero()[1])
-        with open(os.path.join(BASE, os.path.join(ds, 'matrix_ix_to_val.json'))) as f2:
-            ix_to_val = json.load(f2)
-            most_rel = [(val, ix_to_val[str(ix)].encode("utf8")) for val, ix in most_rel if val > t]
-        f.close()
-        f2.close()
-        gc.collect()
-        return most_rel
+    cos_sim = (f.dot(query_vec.T)).T
+    most_rel = zip(cos_sim.data, cos_sim.nonzero()[1])
+    most_rel = [(val, f2[str(ix)].encode("utf8")) for val, ix in most_rel if val > t]
 
+    return most_rel
+#################################################################################################
 
 def reu_id_decomp(reuters_ids):
     """
@@ -127,7 +122,8 @@ def reu_id_decomp(reuters_ids):
     return (d, reu_ids)
 
 
-def complete_search(query):
+def complete_search(query, tf_idf_npz_reu, reu_ix_to_val, id_to_reu, red_vocab_to_ix, \
+ reu_vocab_to_ix, words_compressed, docs_compressed, ATN_word_to_ix, reddit_ix_to_val, date_to_id, red_text):
     """
     Given a user query, return large cards where
     reddit links to reuters headlines as well as
@@ -135,17 +131,12 @@ def complete_search(query):
     (That had no reddit match)
     """
     print >> sys.stderr, "complete search start"
-    reddit_ixs = return_red_ixs(query, 'reddit', t=3)
-    reuters_ids = set(return_reu_ids(query, 'reuters', t=3))
+    reddit_ixs = return_red_ixs(query, 'reddit', 3, red_vocab_to_ix, words_compressed, docs_compressed, ATN_word_to_ix)
+    reuters_ids = set(return_reu_ids(query, 'reuters', 3, tf_idf_npz_reu, reu_ix_to_val, reu_vocab_to_ix, \
+        words_compressed, docs_compressed, ATN_word_to_ix))
 
     #Relevance bootstrap with SVD
-    with open(os.path.join(BASE, os.path.join('reuters', 'id_to_reu_headline.csv'))) as f3:
-        print >> sys.stderr, "loading f3"
-        id_to_reu= pandas.read_csv(f3)
-        print >> sys.stderr, "loaded f3"
-        f3.close()
-        gc.collect()
-    red_text = pickle.load(open(os.path.join(BASE, 'red_ix_to_text.p'), 'rb'))
+    print >> sys.stderr, "before da pickle"
     reuters_ids, reddit_ixs = find_coherent_set(id_to_reu, red_text, reuters_ids, reddit_ixs)
     red_text = None
     gc.collect()
@@ -154,52 +145,40 @@ def complete_search(query):
     reu_id_dict, reu_id_set = reu_id_decomp(reuters_ids)
     print >> sys.stderr, "decomped"
     cards = []
-    with open(os.path.join(BASE, os.path.join('reddit', 'matrix_ix_to_val.json'))) as f1:
-        with open(os.path.join(BASE, os.path.join('reuters', 'date_to_id.json'))) as f2:
-            print >> sys.stderr, "opened files"
-            reddit_ix_to_val = json.load(f1)
-            date_to_id = json.load(f2)
-            print >> sys.stderr, "loaded files"
-            #Iterate over reddit tuples (i.e. big cards)
-            for val, ix in reddit_ixs:
-                tup = reddit_ix_to_val[str(ix)]
+    #Iterate over reddit tuples (i.e. big cards)
+    for val, ix in reddit_ixs:
+        tup = reddit_ix_to_val[str(ix)]
 
-                baddate= tup[0]
-                baddate= string.split(baddate, '/')
-                reddate= baddate[2]
-                if len(baddate[0]) == 1:
-                    reddate += "0" + baddate[0]
-                else:
-                    reddate += baddate[0]
-                if len(baddate[1]) == 1:
-                    reddate += "0" + baddate[1]
-                else:
-                    reddate += baddate[1]
+        baddate= tup[0]
+        baddate= string.split(baddate, '/')
+        reddate= baddate[2]
+        if len(baddate[0]) == 1:
+            reddate += "0" + baddate[0]
+        else:
+            reddate += baddate[0]
+        if len(baddate[1]) == 1:
+            reddate += "0" + baddate[1]
+        else:
+            reddate += baddate[1]
 
-                tup_ixs = date_to_id.get(reddate.encode("utf8"), -1)
-                #Reddit has date, but reuters does not
-                if tup_ixs == -1:
-                    #print("IN -1")
-                    card = [reddate.encode("utf8"), tup[1].encode("utf8"), tup[2], tup[3], tup[4].encode("utf8"), [], round(val,2)]
-                    cards.append(card)
-                    continue
-                tup_ixs = set([date.encode("utf8") for date in tup_ixs])
-                inter = reu_id_set.intersection(tup_ixs)
-                card = [reddate.encode("utf8"), tup[1].encode("utf8"), tup[2], tup[3], tup[4].encode("utf8"), inter, round(val,2)]
-                reu_id_set -= set(tup_ixs)
-                cards.append(card)
-            f1.close()
-            f2.close()
-            gc.collect()
-            #Iterate over reuter ixs not covered through reddit (i.e. small cards)
-            for reu_id in reu_id_set:
-                date = str(reu_id)[:8]
-                card = [date, reu_id, round(reu_id_dict[reu_id], 2)]
-                cards.append(card)
+        tup_ixs = date_to_id.get(reddate.encode("utf8"), -1)
+        #Reddit has date, but reuters does not
+        if tup_ixs == -1:
+            #print("IN -1")
+            card = [reddate.encode("utf8"), tup[1].encode("utf8"), tup[2], tup[3], tup[4].encode("utf8"), [], round(val,2)]
+            cards.append(card)
+            continue
+        tup_ixs = set([date.encode("utf8") for date in tup_ixs])
+        inter = reu_id_set.intersection(tup_ixs)
+        card = [reddate.encode("utf8"), tup[1].encode("utf8"), tup[2], tup[3], tup[4].encode("utf8"), inter, round(val,2)]
+        reu_id_set -= set(tup_ixs)
+        cards.append(card)
     gc.collect()
-
-
-    gc.collect()
+    #Iterate over reuter ixs not covered through reddit (i.e. small cards)
+    for reu_id in reu_id_set:
+        date = str(reu_id)[:8]
+        card = [date, reu_id, round(reu_id_dict[reu_id], 2)]
+        cards.append(card)
     id_ind= pandas.Index(id_to_reu["id"])
     head_ind= pandas.Index(id_to_reu["headline"])
     print >> sys.stderr, "populating cards with headlines"
@@ -213,7 +192,6 @@ def complete_search(query):
                 loc= id_ind.get_loc(each_id)
                 list_headlines.append(head_ind[loc].encode("utf8"))
             each_card[5]= list_headlines
-        print >> sys.stderr, each_card
     cards.sort(key=lambda x: x[-1], reverse=True)
     gc.collect()
     return cards
